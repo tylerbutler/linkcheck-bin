be619b5bc631025c2a47517653e8fa393cf71dfb
Tyler Butler <tyler@tylerbutler.com>
2023-02-07 09:26:02 -0800

    Use checksum URLs

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import path from "path";
-import { fileURLToPath } from "url";
-import { getBinFilename } from "./lib/utils.js";
-
-// https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c#what-do-i-use-instead-of-__dirname-and-__filename
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { __dirname, getBinFilename } from "./lib/utils.js";
 
 const linkcheck = path.join(
   __dirname,
