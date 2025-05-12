e15142281e4dd8b08beceb44d6dca21f01b5c6e0
Tyler Butler <tyler@tylerbutler.com>
2025-05-12 13:53:15 -0700

    fix: Use shell: true on Windows (#57)

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,17 +1,42 @@
 /* eslint-env node, mocha */
-import { execFile } from "child_process";
 import assert from "assert";
+import { execFile } from "child_process";
+import { existsSync } from "fs";
 import bin from "../index.js";
+import { getBinVersion } from "../lib/utils.js";
 
-it("linkcheck exists and runs?", async function () {
-  this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
+it("linkcheck exists", async () => {
+	console.log(bin);
+	assert(existsSync(bin), "linkcheck binary does not exist");
+});
 
-  console.log(bin);
+it("getBinVersion returns the correct version", async () => {
+	const version = getBinVersion(bin);
+	assert.equal(
+		version,
+		"linkcheck version 3.0.0",
+		"linkcheck version does not match",
+	);
+});
 
-  assert(execFile(bin, ["--version"], function (error, stdout) {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  }));
+it("linkcheck runs", async function () {
+	this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
+
+	assert(
+		execFile(
+			bin,
+			["--version"],
+			{
+				// The shell: true option is required on Windows because the Windows wrapper is a batch file.
+				// See https://github.com/nodejs/node/issues/52681#issuecomment-2076426887
+				shell: process.platform === "win32",
+			},
+			(error, stdout) => {
+				if (error) {
+					throw error;
+				}
+				console.log(stdout);
+			},
+		),
+	);
 });
