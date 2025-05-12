47f3a3da8a167a3db692aa9445d444d5ce28f409
Tyler Butler <tyler@tylerbutler.com>
2025-05-12 12:09:58 -0700

    fix test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,14 +4,25 @@ import assert from "assert";
 import bin from "../index.js";
 
 it("linkcheck exists and runs?", async function () {
-  this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
+	this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
 
-  console.log(bin);
+	console.log(bin);
 
-  assert(execFile(bin, ["--version"], function (error, stdout) {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  }));
+	assert(
+		execFile(
+			bin,
+			["--version"],
+			{
+				// The shell: true option is required on Windows because the Windows wrapper is a batch file.
+				// See https://github.com/nodejs/node/issues/52681#issuecomment-2076426887
+				shell: process.platform === "win32",
+			},
+			(error, stdout) => {
+				if (error) {
+					throw error;
+				}
+				console.log(stdout);
+			},
+		),
+	);
 });
